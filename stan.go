d438bee8bf333c3f17ee5e330e5525833cc6876f
Asim Aslam <asim@aslam.me>
2019-06-27 09:49:39 +0100

    Go fmt and update go mod

--- stan.go
+++ stan.go
@@ -11,8 +11,8 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/micro/go-micro/broker"
-	"github.com/micro/go-micro/config/cmd"
 	"github.com/micro/go-micro/codec/json"
+	"github.com/micro/go-micro/config/cmd"
 	log "github.com/micro/go-micro/util/log"
 	stan "github.com/nats-io/stan.go"
 )
